d30d575345da211ff24ff88bb0ae93c420c0008f
atahiri <atahiri@student.1337.ma>
2022-05-13 12:38:01 +0100

    refactoring code

--- pages/api/data/index.js
+++
@@ -1,9 +0,0 @@
-const convert = require('xml-js');
-
-export default async function handler(request, response) {
-    console.log(request.query.url);
-    const res = await fetch(`https://data.alexa.com/data?cli=10&dat=s&url=${"http://alhudabook.com"}`);
-    const data = await res.text();
-    const result = convert.xml2json(data, { compact: true, spaces: 4 });
-    response.send(result);
-}
